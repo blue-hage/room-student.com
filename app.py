c72350908ebf36d6a86a07d4a56e9c6ef1f55ce8
blue-hage <mahi.sakou.011047@gmail.com>
2021-02-22 13:08:24 +0900

    combined to one page

--- app.py
+++ app.py
@@ -8,21 +8,6 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
-@app.route("/concept")
-def concept():
-   return render_template("concept.html")
-
-@app.route("/process")
-def process():
-  return render_template("process.html")
-
-@app.route("/contact")
-def contact():
-  return render_template("contact.html")
-
-@app.route("/warning")
-def warning():
-  return render_template("warning.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
